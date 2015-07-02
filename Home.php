d9f4e388f3fefc82da341db572d38ff1e8e258b3
michele.pagnin <michele.pagnin@PENELOPE.ptqs.it>
2015-07-02 10:05:17 +0200

    Aggiunta home page

---
+++ Home.php
@@ -0,0 +1,20 @@
+<?php
+require './Settings.php';
+
+use web\BasePage;
+use web\masterpages\GlobalMasterPage;
+
+class Home extends BasePage
+{
+    public function __construct()
+    {
+        parent::__construct(__DIR__.'/Home.view.php');
+        
+        parent::setMasterPage(new GlobalMasterPage($this));
+        parent::addMasterPagePart('content', 'content');
+        
+        parent::addCss('web/css/Home.css');
+        
+        parent::setPageTitle("Dev.me");
+    }
+}
---
+++ Home.view.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this Home */
+?>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<div id="content">
+    <h1>Dev.me</h1>
+    <p>
+        Select a function
+    </p>
+</div>
